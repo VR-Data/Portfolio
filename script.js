500320c80848a064b0c91c535bf0c2c214f83696
VR-Data <jordan.trigg97@gmail.com>
2025-01-12 18:19:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -70,9 +70,9 @@
     });
     
     let menuIcon = document.querySelector('#menu-icon');
-    let navbar = document.querySelector('.navbar')
-
-    menuIcon.onClick = () => {
-      menuIcon.classList.toggle('bx-x')
+    let navbar = document.querySelector('.navbar');
+    
+    menuIcon.addEventListener('click', () => {
+      menuIcon.classList.toggle('bx-x');
       navbar.classList.toggle('active');
-    }
+    });
